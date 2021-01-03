ca4d1cd8539df649f07337ff56b5e6992ff405dd
Hans Kristian Flaatten <hans.flaatten@evry.com>
2021-01-03 11:27:41 +0100

    bot: return 200 OK for request to root (#17)

--- config.go
+++ config.go
@@ -10,7 +10,7 @@ type Config struct {
 	LogLevel              string `envconfig:"SLACKBOT_LOGLEVEL" default:"warn"`
 	LogFormat             string `envconfig:"SLACKBOT_LOGFORMAT" default:"text"`
 	Port                  string `envconfig:"SLACKBOT_PORT" default:"8080"`
-	Host                  string `envconfig:"SLACKBOT_HOST" default:"127.0.0.1"`
+	Host                  string `envconfig:"SLACKBOT_HOST" default:"0.0.0.0"`
 	EnableSentiment       bool   `envconfig:"SLACKBOT_REACTION_SENTIMENT_ENABLE" default:"false"`
 	SlackSigningSecret    string `envconfig:"SLACK_SIGNING_SECRET" required:"true"`
 	SlackOAuthAccessToken string `envconfig:"SLACK_OAUTH_ACCESS_TOKEN" required:"true"`
